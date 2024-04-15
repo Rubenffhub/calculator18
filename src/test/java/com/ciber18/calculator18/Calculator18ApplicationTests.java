a5854b402d54c3fb3d934abe9b18880d73b20c4c
Rubenffhub <151557621+Rubenffhub@users.noreply.github.com>
2024-04-15 16:13:47 +0200

    Add files via upload

---
+++ src/test/java/com/ciber18/calculator18/Calculator18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber18.calculator18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
