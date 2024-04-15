eed12156f1704a3a18ea3dff02b6c6eab6740966
rubenff <seguridadrff@gmail.com>
2024-04-15 16:57:04 +0200

    añadido esqueleto spring boot

---
+++ src/main/java/com/ciber18/calculator18/Calculator18Application.java
@@ -0,0 +1,13 @@
+package com.ciber18.calculator18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator18Application.class, args);
+	}
+
+}
